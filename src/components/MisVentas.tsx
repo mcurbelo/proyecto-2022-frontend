d6ae65c92d0808f0b7a99bdca66e2c0d0d00b7b1
nicosposito <nicolas.sposito16@hotmail.com>
2022-11-03 04:42:15 -0300

    Inicio

--- src/components/MisVentas.tsx
+++ src/components/MisVentas.tsx
@@ -1,23 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { Card, List, Input, Space, Image, Steps, Select, DatePicker, DatePickerProps, Empty, Pagination, Tooltip, Row, Divider, Modal } from 'antd';
-import { ExclamationCircleOutlined, SearchOutlined } from '@ant-design/icons';
-import { CompartidoUsuario, CompradorService } from "shopit-shared";
-import { DtCompraSlimComprador, EstadoCompra } from "shopit-shared/dist/user/VendedorService";
-import { DtFiltrosCompras } from "shopit-shared/dist/user/CompradorService";
+import { Card, List, Input, Space, Image, Steps, Select, DatePicker, DatePickerProps, Empty, Pagination, Tooltip, Row, Divider, Modal, message, Dropdown } from 'antd';
+import { ExclamationCircleOutlined, SearchOutlined, UserOutlined } from '@ant-design/icons';
+import { CompartidoUsuario, VendedorService } from "shopit-shared";
+import { DtCompraSlimVendedor, DtFiltrosVentas, EstadoCompra } from "shopit-shared/dist/user/VendedorService";
 import { createUseStyles } from "react-jss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare, faSquareCheck, faStarHalfStroke } from "@fortawesome/free-solid-svg-icons";
-import { faQuestionCircle } from "@fortawesome/free-regular-svg-icons";
+import { faSquareCheck, faStarHalfStroke } from "@fortawesome/free-solid-svg-icons";
+import { faCircleXmark, faHandshake, faQuestionCircle } from "@fortawesome/free-regular-svg-icons";
 import RealizarReclamo from "./RealizarReclamo";
 import RealizarCalificacion from "./RealizarCalificacion";
 import Button from 'antd-button-color';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import 'antd-button-color/dist/css/style.css'; // or 'antd-button-color/dist/css/style.less'
-
+import type { MenuProps } from 'antd';
 
 interface AppState {
-    compras: DtCompraSlimComprador[],
-    filtros: DtFiltrosCompras
+    ventas: DtCompraSlimVendedor[],
+    filtros: DtFiltrosVentas
 }
 
 const { Option } = Select;
@@ -82,11 +81,10 @@ export const MisCompras: React.FC<{}> = () => {
     const id = localStorage.getItem("uuid");
     const token = localStorage.getItem("token");
     const { Step } = Steps;
-    const [compras, setCompras] = useState<AppState["compras"]>()
+    const [ventas, setVentas] = useState<AppState["ventas"]>()
     const [filtros, setFiltros] = useState<AppState["filtros"]>({
         fecha: undefined,
-        nombreVendedor: undefined,
-        nombreProducto: undefined,
+        nombre: undefined,
         estado: undefined
     })
     const [valoresOrdenamiento, setValoresOrdenamiento] = useState({
@@ -117,9 +115,9 @@ export const MisCompras: React.FC<{}> = () => {
     }, [paginaAbuscar])
 
     const busqueda = () => {
-        CompradorService.listarCompras(id!, token!, paginaAbuscar.toString(), valoresOrdenamiento.cantidadItems, valoresOrdenamiento.ordenamiento, valoresOrdenamiento.dirOrdenamiento, filtros).then((result) => {
-            if (result.compras !== undefined) {
-                setCompras(result.compras);
+        VendedorService.listarMisVentas(id!, token!, paginaAbuscar.toString(), valoresOrdenamiento.cantidadItems, valoresOrdenamiento.ordenamiento, valoresOrdenamiento.dirOrdenamiento, filtros).then((result) => {
+            if (result.ventas !== undefined) {
+                setVentas(result.ventas);
                 setInfoPaginacion({ paginaActual: result.currentPage + 1, paginasTotales: result.totalPages * 10, totalItems: result.totalItems })
             }
         })
@@ -180,31 +178,72 @@ export const MisCompras: React.FC<{}> = () => {
         botonCalificar?.setAttribute("disabled", "true");
     }
 
-    const completarCompra = (id: string) => {
+    const completarVenta = (idVenta: string, esEnvio: boolean) => {
 
         confirm({
             title: 'Estás seguro que desea completar esta compra?',
             icon: <ExclamationCircleOutlined />,
             content: 'Al confirmar ',
             onOk() {
-                return CompartidoUsuario.completarEnvio(id, token!).then((result) => {
-                    if (result == "200") {
-                        Modal.success({
-                            title: "Acción exitosa",
-                            content: 'Estado de la compra actualizado exitosamente',
-                        });
-                    } else {
-                        Modal.error({
-                            title: 'Error',
-                            content: 'Ha ocurrido un error inesperado',
-                        });
-                    }
+                if (esEnvio) {
+                    return CompartidoUsuario.completarEnvio(idVenta, token!).then((result) => {
+                        if (result == "200") {
+                            Modal.success({
+                                title: "Acción exitosa",
+                                content: 'Estado de la venta actualizado exitosamente',
+                            });
+                        } else {
+                            Modal.error({
+                                title: 'Error',
+                                content: 'Ha ocurrido un error inesperado',
+                            });
+                        }
 
-                })
+                    })
+                } else {
+                    return VendedorService.completarVentaRetiro(id!, token!, idVenta).then((result) => {
+                        if (result == "200") {
+                            Modal.success({
+                                title: "Acción exitosa",
+                                content: 'Estado de la venta actualizado exitosamente',
+                            });
+                        } else {
+                            Modal.error({
+                                title: 'Error',
+                                content: 'Ha ocurrido un error inesperado',
+                            });
+                        }
+
+                    })
+                }
             },
             onCancel() { },
         });
     }
+
+    const handleMenuClick: MenuProps['onClick'] = e => {
+        message.info('Click on menu item.');
+        console.log('click', e);
+    };
+
+
+    const opciones: MenuProps['items'] = [
+        {
+            label: 'Aceptar',
+            key: '1',
+            icon: <FontAwesomeIcon icon={faHandshake} />,
+        },
+        {
+            label: 'Rechazar',
+            key: '2',
+            icon: <FontAwesomeIcon icon={faCircleXmark} />,
+        },
+    ];
+
+    const menuProps = {
+        opciones,
+        onClick: handleMenuClick,
+    };
 
 
     return (
@@ -213,12 +252,8 @@ export const MisCompras: React.FC<{}> = () => {
                 <Card>
                     <Row style={{ gap: "20px" }}>
                         <div style={{ minWidth: "180px" }} className={styles.filtros}>
-                            <label htmlFor="nProd" style={{ display: "block" }}>Producto:</label>
-                            <Input id="nProd" placeholder="Buscar" onChange={(e) => handleInputChange(e, "nombreProducto")} prefix={<SearchOutlined />} />
-                        </div>
-                        <div style={{ minWidth: "180px" }} className={styles.filtros}>
                             <label htmlFor="nVen" style={{ display: "block" }}>Vendedor:</label>
-                            <Input id="nVen" placeholder="Buscar" onChange={(e) => handleInputChange(e, "nombreVendedor")} prefix={<SearchOutlined />} />
+                            <Input id="nVen" placeholder="Buscar" onChange={(e) => handleInputChange(e, "nombre")} prefix={<SearchOutlined />} />
                         </div>
 
                         <div style={{ minWidth: "180px" }} className={styles.filtros}>
@@ -263,10 +298,10 @@ export const MisCompras: React.FC<{}> = () => {
                     xl: 1,
                     xxl: 1,
                 }}
-                dataSource={compras}
+                dataSource={ventas}
                 renderItem={item => (
                     <List.Item>
-                        <Card title={"Realizada el " + item.fecha.toString()} bodyStyle={{ background: "#f0f2f5" }}>
+                        <Card title={"Iniciada el " + item.fecha.toString()} bodyStyle={{ background: "#f0f2f5" }}>
                             <Row className={styles.comprasContainer} >
                                 <Steps style={{ marginTop: "10px", width: "84%" }} size="small" current={stepCompra(item.estadoCompra)}>
                                     <Step title="Esperando confirmación" />
@@ -282,10 +317,6 @@ export const MisCompras: React.FC<{}> = () => {
 
                                     <div className={styles.divTitulo} style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
                                         <p style={{ font: "menu", textAlign: "justify", textJustify: "inter-word" }}>{item.nombreProducto}</p>
-                                    </div>
-                                    <div className={styles.divPequeño} style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-                                        <p style={{ font: "revert-layer" }}>{item.nombreVendedor}</p>
-                                        <a onClick={iniciarChat}>Iniciar chat</a>
                                     </div>
                                     <div className={styles.divPequeño} style={{ display: "flex", flexDirection: "column", alignItems: "baseline", justifyContent: "center", width: "13%" }}>
                                         <Space direction="vertical">
@@ -303,15 +334,15 @@ export const MisCompras: React.FC<{}> = () => {
                                         <Space direction="vertical" size={15}>
                                             <div style={{ display: "flex", alignItems: "center" }}>
                                                 <Tooltip title="Solo se puede reclamar cuando la compra haya sido confirmada y se esté dentro de la garantía."> <FontAwesomeIcon type="regular" color="#17a2b8" style={{ marginRight: "5px" }} icon={faQuestionCircle} /> </Tooltip>
-                                                <Button style={{ width: "170px" }} disabled={item.estadoCompra == EstadoCompra.EsperandoConfirmacion || item.estadoCompra == EstadoCompra.Cancelada } type="primary" onClick={() => { setMostrarReclamo({ mostrar: true, id: item.idCompra, nombreUsuario: item.nombreVendedor }) }}><b>Realizar reclamo</b> <FontAwesomeIcon icon={faPenToSquare} style={{ display: "inline-block", marginLeft: "10px" }} /></Button>
+                                                <Dropdown.Button menu={menuProps}>Acciones</Dropdown.Button>
                                             </div>
                                             <div style={{ display: "flex", alignItems: "center" }}>
                                                 <Tooltip title="Solo se puede calificar una vez y cuando se haya completado la compra."> <FontAwesomeIcon type="regular" color="#17a2b8" style={{ marginRight: "5px" }} icon={faQuestionCircle} /> </Tooltip>
-                                                <Button style={{ width: "170px" }} disabled={!item.puedeCalificar} id={item.idCompra + "Calificar"} type="warning" onClick={() => { setMostrarCalificar({ mostrar: true, id: item.idCompra, nombreUsuario: item.nombreVendedor, idBoton: item.idCompra + "Calificar" }) }}><b>Calificar</b> <FontAwesomeIcon icon={faStarHalfStroke} style={{ display: "inline-block", marginLeft: "10px" }} /></Button>
+                                                <Button style={{ width: "170px" }} disabled={!item.puedeCalificar} id={item.idCompra + "Calificar"} type="warning" onClick={() => { setMostrarCalificar({ mostrar: true, id: item.idCompra, nombreUsuario: item.nombreComprador, idBoton: item.idCompra + "Calificar" }) }}><b>Calificar</b> <FontAwesomeIcon icon={faStarHalfStroke} style={{ display: "inline-block", marginLeft: "10px" }} /></Button>
                                             </div>
                                             <div style={{ display: "flex", alignItems: "center" }}>
                                                 <Tooltip title="Solo se puede completar compras de tipo envío, una vez superada la fecha estimada de entrega."> <FontAwesomeIcon type="regular" color="#17a2b8" style={{ marginRight: "5px" }} icon={faQuestionCircle} /> </Tooltip>
-                                                <Button disabled={!item.puedeCompletar} style={{ width: "170px" }} type="success" onClick={() => completarCompra(item.idCompra)}> <b>Completar compra</b> <FontAwesomeIcon icon={faSquareCheck} style={{ display: "inline-block", marginLeft: "10px" }} /></Button>
+                                                <Button disabled={!item.puedeCompletar} style={{ width: "170px" }} type="success" onClick={() => completarVenta(item.idCompra, item.esEnvio)}> <b>Completar venta</b> <FontAwesomeIcon icon={faSquareCheck} style={{ display: "inline-block", marginLeft: "10px" }} /></Button>
                                             </div>
                                         </Space>
                                     </div>
