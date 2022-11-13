14dcf90dd54644e4b314021208048a677c84914f
nicosposito <nicolas.sposito16@hotmail.com>
2022-11-13 03:23:26 -0300

    Listar solicitudes terminado

--- src/components/ListarSolicitudes.tsx
+++ src/components/ListarSolicitudes.tsx
@@ -1,16 +1,18 @@
-import { SearchOutlined } from "@ant-design/icons";
-import { faCircleQuestion, faSquareCheck, faComments, faMoneyBillTransfer } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Avatar, Card, Collapse, Divider, Empty, List, Pagination, Radio, RadioChangeEvent, Rate, Row, Select, Space, Tooltip } from "antd";
-import { useState, useEffect } from "react";
+import { Avatar, Card, Col, Empty, List, Pagination, Radio, RadioChangeEvent, Rate, Row, Select, Space, Tooltip } from "antd";
+import { useState, useEffect, SyntheticEvent } from "react";
 import { createUseStyles } from "react-jss";
 import { AdministradorService } from "shopit-shared";
 import { DtSolicitudPendiente } from "shopit-shared/dist/user/VendedorService";
 import Button from 'antd-button-color';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import 'antd-button-color/dist/css/style.css'; // or 'antd-button-color/dist/css/style.less'
-import { faCircleCheck, faCircleXmark, faEye } from "@fortawesome/free-regular-svg-icons";
+import { faEye } from "@fortawesome/free-regular-svg-icons";
 import Solicitud from "./VisualizarSolicitud";
+import { StepBackwardOutlined, UserOutlined } from "@ant-design/icons";
+import userDefault from "../images/user.png"
+
+
 
 interface AppState {
     reclamos: DtSolicitudPendiente[],
@@ -27,7 +29,7 @@ const useStyles = createUseStyles({
     },
 
     divTitulo: {
-        width: "15%"
+        width: "20%"
     },
 
     divPequeño: {},
@@ -35,7 +37,7 @@ const useStyles = createUseStyles({
     container: {
         width: "80%",
     },
-    reclamosContainer: {
+    solicitudContainer: {
         width: "100%",
         justifyContent: "space-around"
     },
@@ -100,6 +102,18 @@ export const Solicitudes = () => {
         setValoresOrdenamiento({ ...valoresOrdenamiento, "dirOrdenamiento": e.target.value })
     };
 
+    const quitarSolicitud = (solicitud: DtSolicitudPendiente) => {
+        const solicitudesReclamoNueva = solicitudes!.slice();
+        const indice = solicitudesReclamoNueva.indexOf(solicitud);
+        solicitudesReclamoNueva.splice(indice, 1)
+        setSolicitudes(solicitudesReclamoNueva);
+    }
+
+    //const handleBrokenImage = (e: ) => (e.currentTarget.src = placeholderLink);
+    const onErrorImage = ((e: any) => {
+        e.target.src = <StepBackwardOutlined />
+    })
+
     document.body.style.backgroundColor = "#F0F0F0"
     return (
         <div style={{ display: "flex", justifyContent: "center", backgroundColor: "#F0F0F0" }}>
@@ -128,7 +142,7 @@ export const Solicitudes = () => {
                         md: 1,
                         lg: 1,
                         xl: 1,
-                        xxl: 1,
+                        xxl: 2,
                     }}
                     dataSource={solicitudes}
                     renderItem={item => (
@@ -140,50 +154,36 @@ export const Solicitudes = () => {
                                     </Button>
                                 </div>
                             }>
-                                <Row className={styles.reclamosContainer} >
-                                    <Row gutter={[0, 20]} className={styles.reclamosContainer} >
-                                        <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                <Row className={styles.solicitudContainer} >
+                                    <Row gutter={[10, 25]} className={styles.solicitudContainer} >
+                                        <Col style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
                                             <Avatar
-                                                size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
-                                                src={item.imagenPerfil}
+                                                size={100}
+                                                src={(item.imagenPerfil != "") ? item.imagenPerfil : userDefault}
                                             />
-                                        </div>
+                                        </Col>
 
-                                        <div className={styles.divTitulo} style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                                            <Row >
-                                                <p style={{ textAlign: "justify", textJustify: "inter-word" }}>{item.nombreApellido}</p>
+                                        <Col className={styles.divPequeño} style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", minWidth: "200px" }}>
+
+                                            <Row>
+                                                <p>{item.nombreApellido}</p>
+                                            </Row>
+                                            <Row>
                                                 <Tooltip title={"Calificación: " + item.calificacion + "/5"} placement="bottom">
                                                     <Rate allowHalf disabled defaultValue={item.calificacion} />
                                                 </Tooltip>
                                             </Row>
-                                        </div>
 
-                                        <div className={styles.divPequeño} style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                                        </Col>
+
+                                        <Col className={styles.divPequeño} style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minWidth: "200px" }}>
                                             <Space direction="vertical">
                                                 <div>
                                                     <p>{"Correo: " + item.correo}</p>
                                                     <p>Teléfono: {(item.telefono) ? item.telefono : "-"}</p>
                                                 </div>
                                             </Space>
-                                        </div>
-
-                                        <div className={styles.divPequeño} style={{ display: "flex", flexDirection: "column", alignItems: "baseline", justifyContent: "center" }}>
-                                            <Space direction="vertical" size={30}>
-                                                <div style={{ display: "flex", alignItems: "center" }}>
-
-                                                    <Button style={{ width: "170px", textShadow: "0 0 2px black" }}
-                                                        type="success"> <b>Aceptar</b> <FontAwesomeIcon icon={faCircleCheck} style={{ display: "inline-block", marginLeft: "10px" }} />
-                                                    </Button>
-                                                </div>
-
-                                                <div style={{ display: "flex", alignItems: "center" }}>
-
-                                                    <Button style={{ width: "170px", textShadow: "0 0 2px black" }}
-                                                        type="danger"> <b>Rechazar</b> <FontAwesomeIcon icon={faCircleXmark} style={{ display: "inline-block", marginLeft: "10px" }} />
-                                                    </Button>
-                                                </div>
-                                            </Space>
-                                        </div>
+                                        </Col>
                                     </Row>
                                 </Row>
 
@@ -193,7 +193,7 @@ export const Solicitudes = () => {
                 />
                 <Pagination hideOnSinglePage style={{ display: 'flex', justifyContent: 'center', marginTop: '3%' }} defaultCurrent={infoPaginacion.paginaActual} total={infoPaginacion.paginasTotales} current={infoPaginacion.paginaActual} onChange={(value) => { setPaginaAbuscar(value - 1); window.scrollTo({ top: 0, behavior: 'auto' }) }} />
                 {
-                    (mostrarVisualizarSolicitud!.mostrar) ? <Solicitud solicitud={mostrarVisualizarSolicitud?.solicitud!} showModal={() => { setVisualizarSolicitud({ solicitud: undefined, mostrar: false }) }} ></Solicitud> : null
+                    (mostrarVisualizarSolicitud!.mostrar) ? <Solicitud solicitud={mostrarVisualizarSolicitud?.solicitud!} quitar={() => quitarSolicitud(mostrarVisualizarSolicitud?.solicitud!)} showModal={() => { setVisualizarSolicitud({ solicitud: undefined, mostrar: false }) }} ></Solicitud> : null
                 }
             </div >
         </div >
